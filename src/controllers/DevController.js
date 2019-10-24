7df1f6368dd9c250f5b8188cbec3e648324962f1
osmar <ojw@pinhaotec.com.br>
2019-10-24 14:59:41 -0300

    OsmarJoseph

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -14,12 +14,24 @@ module.exports = {
 
         const {name,bio,avatar_url:avatar} = userProfile.data
 
-        const devPeronSaved = await Dev.create({
+        const devPersonSaved = await Dev.create({
             name,
             user : username,
             bio,
             avatar
         })
-        return res.json(devPeronSaved)
+        return res.json(devPersonSaved)
+    },
+    async index(req,res){
+        const {user:loggedUserId} = req.headers;
+        const loggedDev = await Dev.findById(loggedUserId)
+        const allUsers = await Dev.find({
+            $and:[
+                {_id: {$ne :loggedUserId}},
+                {_id: {$nin :loggedDev.likes}},
+                {_id: {$nin :loggedDev.dislikes}},
+            ]
+        })
+        return res.json(allUsers)
     }
 };
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 const server = express();
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-rtqmk.mongodb.net/omnistack8?retryWrites=true&w=majority',{
@@ -8,6 +9,7 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-rtqmk.mongodb.net/o
     useUnifiedTopology: true 
 })
 
+server.use(cors())
 server.use(express.json())
 server.use(routes);
 
