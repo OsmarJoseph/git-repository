c25b913452d37a791dfbce44b792c3b545fe0b62
OsmarJoseph <osmarjoseph2013@hotmail.com>
2019-11-20 22:59:28 -0300

    socket

--- src/server.js
+++ src/server.js
@@ -2,15 +2,23 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 const cors = require('cors');
-const server = express();
+const httpServer = express();
+const server = require('http').Server(httpServer);
+// return a fuction
+const io = require('socket.io')(server);
+
+io.on('connection',socket =>{
+    console.log('socket',socket.id);
+})
+
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-rtqmk.mongodb.net/omnistack8?retryWrites=true&w=majority',{
     useNewUrlParser: true,
     useUnifiedTopology: true 
 })
 
-server.use(cors())
-server.use(express.json())
-server.use(routes);
+httpServer.use(cors())
+httpServer.use(express.json())
+httpServer.use(routes);
 
 server.listen('3333');
